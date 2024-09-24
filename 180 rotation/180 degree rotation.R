e9fc61b35a1f0ee73c89a14613f4c836932bf21a
anjali <rhkrrn258369@gmail.com>
2024-09-24 14:03:27 +0530

    Add files via upload

---
+++ 180 rotation/180 degree rotation.R
@@ -0,0 +1,16 @@
+
+#rotating a imagecol.mat=as.array(img[,,1,])
+
+library(imager)
+img=load.image("boat.jpeg")
+col.mat=as.array(img[,,1,])
+dims=dim(col.mat)
+rotate=array(0,dim=dims)
+for(i in 1:dims[1]){
+  for(j in 1:dims[2]){
+  rotate[i,j,]=col.mat[dims[1]-i+1,dims[2]-j+1,]
+  }
+}
+par(mfrow=c(1,2))
+plot(as.cimg(rotate))
+plot(img)
